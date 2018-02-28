f0bfd2a6bb7586c645fdcb76a84a6c74fb5bcdd1
Victor Nidens <victor.nidens@gmail.com>
2018-02-28 22:15:43 +0300

    Added solutions for collections koans

--- src/ii_collections/n21Partition.kt
+++ src/ii_collections/n21Partition.kt
@@ -12,5 +12,9 @@ fun example8() {
 
 fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
     // Return customers who have more undelivered orders than delivered
-    todoCollectionTask()
+    return customers.partition {
+        it.orders.count() - it.orders.count { it.isDelivered } > 0
+    }
+            .first
+            .toSet()
 }
